567670d7cadc46c3cdd02e32d743e2d848d31405
stucci <sts.ucd@gmail.com>
2019-05-24 13:34:06 +0900

    delete comment out line

--- ClipChrome--app/ClipChrome--app.cs
+++ ClipChrome--app/ClipChrome--app.cs
@@ -15,15 +15,11 @@ class ClipSearch
         clipboardText = clipboardText.Trim();
 
         if (Regex.IsMatch(clipboardText, @"^https?://[-_.!~*'()a-zA-Z0-9;/?:@&=+$,%#]+$")) {
-            // || Regex.IsMatch(clipboardText, @"^\\")) {
             url = "\"--app=" + clipboardText + "\"";
         } else {
-            // clipboardText = Regex.Replace( clipboardText, "\\s+", "+" );
-            // url = "https://www.google.co.jp/search?q=" + clipboardText;
             return;
         }
         Process.Start(@"chrome", url);
-        // Process.Start(@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", url);
 //        Console.WriteLine( url );
 //        Console.ReadLine();
     }
